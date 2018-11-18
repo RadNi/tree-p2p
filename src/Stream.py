2c48bd7d713a3e79614135ea9d788399c53be509
RadNi <amirhossein_kh@ymail.com>
2018-11-18 11:38:13 +0330

    Some of stream class methods implemented. a little bug fix on doc.

--- src/Stream.py
+++ src/Stream.py
@@ -1,4 +1,4 @@
-from simpletcp.tcpserver import TCPServer
+from src.tools.simpletcp.tcpserver import TCPServer
 
 from src.tools.simpletcp.clientsocket import ClientSocket
 
@@ -7,10 +7,13 @@ class Stream:
 
     def __init__(self, ip, port):
         self.messages_dic = {}
+        self.server_in_buf = {}
+        self.parent = (None, None)  #TODO   fill this later as (ip, port)
 
         def cb(ip, queue, data):
             queue.put(bytes('ACK'))
-            self.messages_dic.update({ip: self.messages_dic.get(ip).append(data)})
+            # self.messages_dic.update({ip: self.messages_dic.get(ip).append(data)})
+            self.server_in_buf.update({ip, data})
 
         self.server = TCPServer(ip, port, cb)
         self.clients = []
@@ -23,3 +26,9 @@ class Stream:
         self.clients.remove(cl)
         cl.close()
         self.messages_dic.pop((cl.connect_ip, cl.connect_port))
+
+    def read_in_buf(self):
+        return self.server_in_buf
+
+    def send_message(self, client, message):
+        self.messages_dic.update({client, self.messages_dic.pop(client).append(message)})
